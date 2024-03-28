2efd90685f0ca822f4bdbc6766583e35be6bbbe5
tanhxwork <81126830+tanhxwork@users.noreply.github.com>
2024-03-28 22:26:12 +0800

    small update

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,17 @@
 @import "~/assets/scss/_colors.scss";
 
 .main {
-  max-width: 80%;
+  // max-width: 80%;
+  // margin: 0 auto;
+  width: 80%;
+  // max-width: 800px;
   margin: 0 auto;
+}
+
+@media (max-width: 768px) {
+  .main {
+    max-width: 100%;
+    padding: 0 20px;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
+  <div class="background-image">
     <h1>Welcome to the homepage</h1>
     <AppAlert> This is an auto-imported component </AppAlert>
   </div>
 </template>
+
+<style scoped>
+.background-image {
+  background-image: url("@/public/img/trading-tango-header.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: unset;
+}
+</style>
